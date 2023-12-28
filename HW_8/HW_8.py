4ae84b721002f762e5f1702ee62be333e8f4fad7
Ruslan Pislyakov <Ruslan.Pislyakov@redsailtechnologies.com>
2023-12-28 07:59:58 -0800

    Add if statement for the epmty list case

--- HW_8/HW_8.py
+++ HW_8/HW_8.py
@@ -2,11 +2,13 @@
 # [6] => 36
 # [] => 0
 
-lst = [1, 3, 5]
+lst = []
 result = 0
 
-for i in range(0, len(lst), 2):
-    result += lst[i]
+if lst:
+    for i in range(0, len(lst), 2):
+        result += lst[i]
 
-result *= lst[-1]
+    result *= lst[-1]
+
 print(result)
