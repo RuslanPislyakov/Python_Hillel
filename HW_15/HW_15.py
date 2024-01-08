a8cdbcf9689144f460d75ee2cf84b8cfaf18f72a
Ruslan Pislyakov <Ruslan.Pislyakov@redsailtechnologies.com>
2024-01-07 23:56:12 -0800

    Homework 15. Added different day string forms depending on the day amount

--- HW_15/HW_15.py
+++ HW_15/HW_15.py
@@ -4,4 +4,14 @@ days, seconds = divmod(init_digit, 24 * 60 * 60)
 hours, seconds = divmod(seconds, 60 * 60)
 minutes, seconds = divmod(seconds, 60)
 
-print(f"{days} днів, {str(hours).zfill(2)}:{str(minutes).zfill(2)}:{str(seconds).zfill(2)}")
+if 11 <= days <= 14:
+    day_print_form = 'днів'
+elif 2 <= days % 10 <= 4:
+    day_print_form = 'дні'Added different day string forms depending on the day amount
+elif days % 10 == 1:
+    day_print_form = 'день'
+else:
+    day_print_form = 'днів'
+
+print(f"{days} {day_print_form}, {str(hours).zfill(2)}:{str(minutes).zfill(2)}:{str(seconds).zfill(2)}")
+
