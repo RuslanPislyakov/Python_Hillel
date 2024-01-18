05e74b2f530a0bc5b1849b8098cd35648344675a
Ruslan Pislyakov <Ruslan.Pislyakov@redsailtechnologies.com>
2024-01-17 21:50:40 -0800

    Homework 23

---
+++ HW_23/HW_23.py
@@ -0,0 +1,14 @@
+def popular_words(text, words):
+    text = text.lower()
+    text_lst = text.split()
+    res_dict = dict.fromkeys(words, 0)
+    for word in text_lst:
+        if word in res_dict:
+            res_dict[word] += 1
+    print(res_dict)
+    return res_dict
+
+
+assert popular_words('''When I was One I had just begun When I was Two I was nearly new ''',
+                     ['i', 'was', 'three', 'near']) == {'i': 4, 'was': 3, 'three': 0, 'near': 0}, 'Test1'
+print('OK')
